992a1d087814f3c1155d52c7d0a2b559bb6b9702
David <davidisaakov@gmail.com>
2024-04-17 02:09:04 -0400

    Updated Cat class to handle only information about cat

--- Cat.java
+++ Cat.java
@@ -1,27 +1,20 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Cat {
     // Fields
     private String name;
     private LocalDate dateOfBirth;
     private double weight;
-    private int totalDosesRequired;
-    private String timeForDose;
-    private Map<LocalDate, Boolean> doseStatus; // Map to track dose status for each day
 
-    public Cat(String name, String dob, double weight, int totalDosesRequired, String timeForDose) {
+    // Constructor
+    public Cat(String name, String dob, double weight) {
         this.name = name;
         this.dateOfBirth = LocalDate.parse(dob, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
         this.weight = weight;
-        this.totalDosesRequired = totalDosesRequired;
-        this.timeForDose = timeForDose;
-        this.doseStatus = new HashMap<>();
     }
 
-
+    // Getters and Setters
     public String getName() {
         return name;
     }
@@ -46,45 +39,13 @@ public class Cat {
         this.weight = weight;
     }
 
-    public int getTotalDosesRequired() {
-        return totalDosesRequired;
-    }
-
-    public void setTotalDosesRequired(int totalDosesRequired) {
-        this.totalDosesRequired = totalDosesRequired;
-    }
-
-    public String getTimeForDose() {
-        return timeForDose;
-    }
-
-    public void setTimeForDose(String timeForDose) {
-        this.timeForDose = timeForDose;
-    }
-
-    public void markDoseGiven() {
-        LocalDate today = LocalDate.now();
-        doseStatus.put(today, true);
-    }
-
-    public boolean isDoseGivenToday() {
-        LocalDate today = LocalDate.now();
-        return doseStatus.getOrDefault(today, false);
-    }
-
-    public void updateTimeForDose(String newTime) {
-        this.timeForDose = newTime;
-    }
-
+    // String Representation
     @Override
     public String toString() {
         return "Cat{" +
-                "name='" + name + '\'' +
-                ", dateOfBirth=" + dateOfBirth.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")) +
-                ", weight=" + weight +
-                ", totalDosesRequired=" + totalDosesRequired +
-                ", timeForDose='" + timeForDose + '\'' +
-                ", doseStatus=" + doseStatus +
-                '}';
+               "name='" + name + '\'' +
+               ", dateOfBirth=" + dateOfBirth.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")) +
+               ", weight=" + weight +
+               '}';
     }
 }
