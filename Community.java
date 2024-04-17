8d1c7fa9cb4ef484ee09fe04301276eda2b85858
David <davidisaakov@gmail.com>
2024-04-17 02:14:56 -0400

    Created basic class to provide a list of links for FIP communities

--- Community.java
+++ Community.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Community {
-    
+    // List of pre-defined links
+    private List<String> links;
+
+    public Community() {
+        links = new ArrayList<>();
+        initializeLinks();
+    }
+
+    // Private method to add all initial links
+    private void initializeLinks() {
+        links.add("https://fipwarriors.com/ - FIP Warrors Support Group");
+        links.add("https://www.facebook.com/groups/fipwarriorsoriginal/ - FIP Warriors Facebook Group");
+    }
+
+    // Public method to get the links
+    public List<String> getLinks() {
+        return new ArrayList<>(links); 
+    }
+
+    // Convert all links to a single string for display (WIP)
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Community Support Links:\n");
+        for (String link : links) {
+            sb.append(link).append("\n");
+        }
+        return sb.toString();
+    }
 }
