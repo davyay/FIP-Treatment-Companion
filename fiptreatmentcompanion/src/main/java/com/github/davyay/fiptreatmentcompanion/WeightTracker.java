77736ff8cf2b76e63d007694cd87d3c28af60d82
David <davidisaakov@gmail.com>
2024-04-24 05:06:47 -0400

    ignored current weight incorrectly serialized

--- fiptreatmentcompanion/src/main/java/com/github/davyay/fiptreatmentcompanion/WeightTracker.java
+++ fiptreatmentcompanion/src/main/java/com/github/davyay/fiptreatmentcompanion/WeightTracker.java
@@ -1,12 +1,14 @@
 package com.github.davyay.fiptreatmentcompanion;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeightTracker {
     private List<WeightRecord> weightRecords;
 
