a1b834660355bc3627c29e2aaeeac14686d27ee3
David <davidisaakov@gmail.com>
2024-04-24 03:48:21 -0400

    Created main class

--- fiptreatmentcompanion/src/main/java/com/github/davyay/fiptreatmentcompanion/Main.java
+++ fiptreatmentcompanion/src/main/java/com/github/davyay/fiptreatmentcompanion/Main.java
@@ -1,5 +1,35 @@
 package com.github.davyay.fiptreatmentcompanion;
 
-public class Main {
-    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/WelcomePage.fxml"));
+            Parent root = loader.load();
+
+            // Get the controller and set the primary stage
+            WelcomePageController controller = loader.getController();
+            controller.setPrimaryStage(primaryStage);
+
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("FIP Treatment Companion");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Start the JavaFX application
+    }
 }
